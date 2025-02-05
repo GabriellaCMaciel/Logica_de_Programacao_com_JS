2186dcf6c5358d3e2111ab61b553f478b4ae9296
Gabriella Maciel <gabriellaacunhamaciel@gmail.com>
2025-02-05 12:46:17 -0300

    Add files via upload

---
+++ Logica_de_Programacao_com_JS/script3.js
@@ -0,0 +1,12 @@
+// String
+let nomePokemon = "Pikachu"
+let pokemonSexo = "M"
+
+// Number
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+// Boolean
+let selecionavel = true
+// ou
+let selecionavel2 = false
